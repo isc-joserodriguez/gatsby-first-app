a342fd7dd38c91a6a928f6b895d319a9c3bd5d70
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-03-01 12:42:24 -0700

    Navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { Link } from 'gatsby';
+import Navbar from '../components/Navbar';
 
 export default () => (
   <div>
+    <Navbar />
     Hello people!
     <div>
       <Link to="/blog">Blog page</Link>
