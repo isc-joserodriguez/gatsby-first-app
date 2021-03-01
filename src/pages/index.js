fa167ab3b480e2c3cea33e8686319e52bf1fd028
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-03-01 12:37:54 -0700

    Link Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import { Link } from 'gatsby';
 
-export default () => <div>Hello people!</div>
+export default () => (
+  <div>
+    Hello people!
+    <div>
+      <Link to="/blog">Blog page</Link>
+    </div>
+    <a href="https://www.gatsbyjs.com">Gatsby docs</a>
+  </div>
+)
