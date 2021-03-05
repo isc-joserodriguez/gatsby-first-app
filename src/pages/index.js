ca55cbae9461f32a9671c3f80d4c31acd87f2b4c
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-03-05 10:17:13 -0700

    Styled Components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
 import Layout from '../components/Layout';
-
+import { ExampleButton } from '../components/button';
 export default () => (
   <Layout>
-    <h1 style={{ color: "red", textTransform: "uppercase" }}>
+    <h1 style={{ color: "green", textTransform: "uppercase" }}>
       Hello from gatsby!
     </h1>
+    <h1>Hello world!</h1>
+    <h1>Hello people!</h1>
+    <ExampleButton>Click Here!</ExampleButton>
   </Layout>
 )
