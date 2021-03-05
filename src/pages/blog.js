79240c82cc25c52f1448c3b7b5ecd5ca814e41d1
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-03-05 08:43:37 -0700

    CSS Modules

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Link } from 'gatsby';
 import Layout from '../components/Layout';
+import classes from '../components/blog.module.css'
 
 const blog = () => {
     return (
         <Layout>
-            <h1>This is our blog page</h1>
+            <div className={classes.page}>
 
-            <p className="blog-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto est quidem enim excepturi inventore beatae assumenda ut magni aliquid voluptatibus laudantium sunt totam natus, quibusdam esse error perspiciatis, ipsa recusandae.
+                <h1>This is our blog page</h1>
+
+                <p className={classes.text}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto est quidem enim excepturi inventore beatae assumenda ut magni aliquid voluptatibus laudantium sunt totam natus, quibusdam esse error perspiciatis, ipsa recusandae.
             </p>
+            </div>
         </Layout>
     )
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import Layout from '../components/Layout';
+import classes from '../components/products.module.css';
 
 export default class products extends Component {
     render() {
         return (
             <Layout>
-                <h1>This is our products page</h1>
-                <p className="products-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto est quidem enim excepturi inventore beatae assumenda ut magni aliquid voluptatibus laudantium sunt totam natus, quibusdam esse error perspiciatis, ipsa recusandae.
+                <div className={classes.page}>
+
+                    <h1>This is our products page</h1>
+                    <p className={classes.text}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto est quidem enim excepturi inventore beatae assumenda ut magni aliquid voluptatibus laudantium sunt totam natus, quibusdam esse error perspiciatis, ipsa recusandae.
                     </p>
+                </div>
             </Layout>
         )
     }
