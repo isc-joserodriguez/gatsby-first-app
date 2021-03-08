cdf38af85362f6657c5baba55fe03d40cd230344
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-03-07 21:22:42 -0700

    PageQuery

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -2,15 +2,36 @@ import React from 'react';
 import Header from '../examples/Header';
 import HeaderStatic from '../examples/HeaderStatic';
 import Layout from '../components/Layout';
+import { graphql } from 'gatsby'
 
-const examples = () => {
+const examples = ({ data }) => {
+    const { site: { info: { author } } } = data
     return (
         <Layout>
             <h1>Hello from examples page</h1>
             <Header />
             <HeaderStatic />
+            <h5>Author: {author}</h5>
         </Layout>
     )
 }
+
+export const data = graphql`
+query MyQuery {
+    site {
+      info:siteMetadata {
+        person {
+          age
+          name
+        }
+        title
+        description
+        data
+        author
+      }
+    }
+  }
+  
+`
 
 export default examples
