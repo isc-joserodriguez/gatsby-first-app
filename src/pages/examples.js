57b4d2fbcad0abfc9cdadca5f11264d5ac1f9ae4
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-03-07 21:14:35 -0700

    StaticQuery Component

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import Header from '../examples/Header'
-import Layout from '../components/Layout'
+import React from 'react';
+import Header from '../examples/Header';
+import HeaderStatic from '../examples/HeaderStatic';
+import Layout from '../components/Layout';
 
 const examples = () => {
     return (
         <Layout>
             <h1>Hello from examples page</h1>
             <Header />
+            <HeaderStatic />
         </Layout>
     )
 }
