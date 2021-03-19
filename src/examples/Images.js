4f7bc58290f870db8e0c5cabc70dadf2da5656d9
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-03-18 21:11:41 -0700

    Render gatsby-image

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -5,32 +5,40 @@ import Image from 'gatsby-image';
 
 const getImages = graphql`
 {
-    file(relativePath: {eq: "desert.jpeg"}) {
+    fixed: file(relativePath: {eq: "desert.jpeg"}) {
       childImageSharp {
-        fixed(width: 300, height: 400) {
-          src
+        fixed(width: 200, grayscale:true) {
+          ...GatsbyImageSharpFixed
         }
       }
     }
     fluid: file(relativePath: {eq: "ocean.jpeg"}) {
       childImageSharp {
         fluid {
-          src
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
         }
       }
     }
   }
   `;
 const Images = () => {
+    const data = useStaticQuery(getImages);
+
     return <section className="images">
         <article className="single-image">
             <h3>basic image</h3>
+            <img src={img} width="100%" />
         </article>
         <article className="single-image">
             <h3>fixed image/blur</h3>
+            <Image fixed={data.fixed.childImageSharp.fixed} />
         </article>
         <article className="single-image">
             <h3>fluid image/svg</h3>
+            <Image fluid={data.fluid.childImageSharp.fluid} />
+            <div className="small">
+                <Image fluid={data.fluid.childImageSharp.fluid} />
+            </div>
         </article>
     </section>
 }
